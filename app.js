dac3c452820f864a6fa4bde1213da2dc9a8246b3
Fabien Daguet <daguet77@gmail.com>
2021-01-11 17:39:59 +0100

    black background for small screen < 768px

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 "use-strict";
 
-const progress = document.querySelectorAll(".progress_bar");
-let pro = document.querySelector(".pro-title");
-let comp = document.querySelector(".comp-title");
-let body = document.querySelector(".body");
+const progress = document.querySelectorAll(".progress-js");
+let pro = document.querySelector(".pro-js");
+let comp = document.querySelector(".comp-js");
+let background = document.querySelector(".background-js");
 
 
     console.log(pro.getBoundingClientRect().top);
@@ -11,28 +11,32 @@ let body = document.querySelector(".body");
 
 function scrollAnimation() {
 
-    if (pro.getBoundingClientRect().top > 585) {
-        body.style.background = "url(img/backpic.jpg) no-repeat center center fixed";
-        body.style.backgroundSize = "cover";
-    } else if (pro.getBoundingClientRect().top <= 585 && pro.getBoundingClientRect().top >= 65.375) {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        if (pro.getBoundingClientRect().top > 585) {
+            background.style.background = "url(img/backpic.jpg) no-repeat center center fixed";
+            background.style.backgroundSize = "cover";
+        } else if (pro.getBoundingClientRect().top <= 585 && pro.getBoundingClientRect().top >= 65.375) {
 
-        body.style.background = "url(img/cerces.jpg) no-repeat center center fixed";
-        body.style.backgroundSize = "cover";
-        body.style.transitionDuration = "0.5s"
-    } 
+            background.style.background = "url(img/cerces.jpg) no-repeat center center fixed";
+            background.style.backgroundSize = "cover";
+            background.style.transitionDuration = "0.5s"
+        } 
 
-    for (let element of progress) {
-        setTimeout(
-            function() {
-                if (comp.getBoundingClientRect().top < 596.5625) {
+        for (let element of progress) {
+            setTimeout(
+                function() {
+                    if (comp.getBoundingClientRect().top < 596.5625) {
 
-                    element.style.width = element.getAttribute("data-done") + "%";
-                    element.style.opacity = 1;
-                    body.style.background = "url(img/code.jpg) no-repeat center center fixed";
-                    body.style.backgroundSize = "cover";
-                } 
-            }, 1000
-        );
+                        element.style.width = element.getAttribute("data-done") + "%";
+                        element.style.opacity = 1;
+                        background.style.background = "url(img/code.jpg) no-repeat center center fixed";
+                        background.style.backgroundSize = "cover";
+                    } 
+                }, 1000
+            );
+        }
+    } else {
+        background.style.backgroundColor = "black";
     }
 }
 
